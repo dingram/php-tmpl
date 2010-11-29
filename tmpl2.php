f0718b1b728558022accc7fdd2d4614168a2d7e8
Dave Ingram <dave@dmi.me.uk>
2010-11-29 10:50:05 +0000

    More temporary tests

--- tmpl2.php
+++ tmpl2.php
@@ -1,18 +1,24 @@
 <?php
 
+/*
 $t = new TextTemplate();
 var_dump($t->compile('abcd<!--{TAG}-->efgh'));
 var_dump($t->render(array('TAG'=>'<TAG_GOES_HERE>')));
 var_dump($t->render(array('TAG'=>' ANOTHER_TAG ')));
 print "\n\n";
+*/
 
-$tmpl = 'abcd<!--{?TAG}-->efgh<!--{TAG}-->ijkl<!--{/?}-->mnop';
-var_dump($tmpl);
-tmpl_parse($tmpl);
+function test_parse($tpl) {
+	print str_repeat('=', 78)."\n";
+	print "$tpl\n";
+	print str_repeat('-', 78)."\n";
+	tmpl_parse($tpl);
+	print str_repeat('=', 78)."\n\n";
+}
 
-/*
-print "\n\n";
-var_dump($t->compile('abcd<!--{?TAG}-->efgh<!--{/?}-->'));
-var_dump($t->render(array('TAG'=>'<TAG_GOES_HERE>')));
-var_dump($t->render(array('TAG'=>' ANOTHER_TAG ')));
- */
+test_parse('<!--{?A}--><!--{/#}-->');
+test_parse('<!--{:}-->efgh<!--{TAG}-->ijkl<!--{/?}-->mnop');
+test_parse('abcd<!--{?TAG}-->efgh<!--{TAG}-->ijkl<!--{/?}-->mnop');
+test_parse('abcd<!--{?TAG}-->efgh<!--{TAG}-->ijkl<!--{:}-->mnop<!--{/?}-->qrst');
+#test_parse('abcd<!--{?TAG}-->efgh<!--{TAG}-->ijkl<!--{:?TAG2}-->mnop<!--{/?}-->qrst');
+#test_parse('abcd<!--{?TAG}-->efgh<!--{TAG}-->ijkl<!--{:?TAG2}-->mnop<!--{:}-->qsrt<!--{/?}-->uvwx');
