4a8e5d1c7413619532f8ee9c30e2d3188939e898
Dave Ingram <dave@dmi.me.uk>
2011-03-17 11:36:17 +0000

    Added new quicktest with string keys and integer values

--- quicktests/tmpl4.php
+++ quicktests/tmpl4.php
@@ -163,3 +163,15 @@ $t->compile($tpl);
 var_dump($t->render(array('ARRY'=>array('foo', 'bar', 'baz', 'quux'))));
 
 print "\n\n";
+
+$t = new TextTemplate();
+$tpl = "\n".
+  TextTemplate::tokenizeLoop('ARRY').
+    TextTemplate::tokenize('$@').': '.TextTemplate::tokenize('$')."\n".
+  TextTemplate::tokenizeLoopEnd();
+
+var_dump($tpl);
+$t->compile($tpl);
+var_dump($t->render(array('ARRY'=>array('foo'=>0, 'bar'=>1, 'baz'=>2, 'quux'=>3))));
+
+print "\n\n";
